9cb3256a00ba5849e582a18ddd94542729388466
Luckyharvey <84315121+Lucky-code-cloud@users.noreply.github.com>
2025-12-27 09:31:43 +0200

    Update menu icon toggle functionality

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.header .navbar');
 
 menu.onclick = () =>{
+    menu.classList.toggle('fa-bars');
     menu.classList.toggle('fa-xmark');
     navbar.classList.toggle('active');
 }
